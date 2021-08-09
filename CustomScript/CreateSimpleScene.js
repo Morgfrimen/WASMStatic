d59797c5d25b639bab60d41c4202fae74eaa08b0
Morgfrimen <44734894+Morgfrimen@users.noreply.github.com>
2021-08-09 14:24:07 +0000

    Deploying to gh-pages from @ Morgfrimen/WASMStatic@7ecf080a3d5d985234a8c0682efd120551c2a8f9 🚀

--- CustomScript/CreateSimpleScene.js
+++ CustomScript/CreateSimpleScene.js
@@ -1,8 +1,8 @@
 ﻿
 
 window.loadScene = function(canvasId) {
-    const width = window.innerWidth -300;
-    const height = window.innerHeight - 300;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
     const canvas = document.getElementById(canvasId);
     canvas.setAttribute('width', width);
     canvas.setAttribute('height', height);
@@ -13,7 +13,7 @@ window.loadScene = function(canvasId) {
     var scene = new THREE.Scene();
 
     var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000);
-    camera.position.set(0, 0, 1500);
+    camera.position.set(1000, 0, 2500);
 
     var light = new THREE.AmbientLight(0xffffff);
     scene.add(light);
@@ -23,15 +23,13 @@ window.loadScene = function(canvasId) {
 
 
     let arrayCube = [];
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 50000; i++) {
       
         var cube = new THREE.Mesh(geometry, material);
-        cube.position.set(-1000 + i*100, (Math.random() * (500 - -500) + -500), 0);
+        cube.position.set(-1000 + i*10, (Math.random() * (1000 - -1000) + -1000), 0);
         arrayCube.push(cube);
         scene.add(cube);
     }
-
-    
 
     function renderAnimation() {
         requestAnimationFrame(renderAnimation);
@@ -43,6 +41,44 @@ window.loadScene = function(canvasId) {
     }
     renderAnimation();
 };
+
+
+window.footerCreate = function(width,height,lenght, stepLenght,stepWidth) {
+    let widthFooter = width;
+    let heightFooter = height;
+    let lenghtFooter = lenght;
+
+    //Дубляж кода - исправить
+    //++++++++++++++++++++++++++++++++
+    const canvas = document.getElementById("treeJSScene");
+    var render = new THREE.WebGLRenderer({ canvas: canvas });
+    render.setClearColor(0x000000);
+
+    var scene = new THREE.Scene();
+
+    var light = new THREE.AmbientLight(0xffffff);
+    scene.add(light);
+
+    const geometry = new THREE.BoxGeometry(1, heightFooter, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x344500 });
+    //++++++++++++++++++++++++++++++++
+
+    let stepLenghtFooter = stepLenght;
+    let stepWidthFooter = stepWidth;
+
+    let countWidth = widthFooter / stepWidthFooter;
+    let countLenght = lenghtFooter / stepLenghtFooter;
+
+
+    for (let lenghtIndex = 0; lenghtIndex < Math.floor(countLenght);lenghtIndex++) {
+        for (let widthIndex = 0; widthIndex < Math.floor(countWidth); widthIndex++) {
+            
+            var cube = new THREE.Mesh(geometry, material);
+            cube.position = new Vector3(lenghtIndex * stepLenghtFooter, 0, widthIndex * stepWidthFooter);
+            scene.add(cube);
+        }
+    }
+}
 
 
 
