5138e7690eb7f4851aeb6e3266ad8bf34331e06b
Morgfrimen <44734894+Morgfrimen@users.noreply.github.com>
2021-08-09 15:20:08 +0000

    Deploying to gh-pages from @ Morgfrimen/WASMStatic@2db7a1532151d15eeadb506f7a3daa4d39d30fb7 🚀

--- CustomScript/CreateSimpleScene.js
+++ CustomScript/CreateSimpleScene.js
@@ -12,21 +12,22 @@ window.loadScene = function(canvasId) {
 
     var scene = new THREE.Scene();
 
-    var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000);
+    var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 15000);
     camera.position.set(1000, 0, 2500);
 
     var light = new THREE.AmbientLight(0xffffff);
     scene.add(light);
 
     const geometry = new THREE.BoxGeometry(100,100,100);
-    const material = new THREE.MeshBasicMaterial({ color: 0x344500 });
+    const material = new THREE.MeshBasicMaterial({ color: 0x345678 });
 
 
     let arrayCube = [];
     for (let i = 0; i < 50000; i++) {
       
         var cube = new THREE.Mesh(geometry, material);
-        cube.position.set(-1000 + i*10, (Math.random() * (1000 - -1000) + -1000), 0);
+        cube.position.set(-1000 + i*10, (Math.random() * (1000 - -1000) + -1000),
+            (Math.random() * (100 - -50) + -50));
         arrayCube.push(cube);
         scene.add(cube);
     }
@@ -44,40 +45,80 @@ window.loadScene = function(canvasId) {
 
 
 window.footerCreate = function(width,height,lenght, stepLenght,stepWidth) {
-    let widthFooter = width;
-    let heightFooter = height;
-    let lenghtFooter = lenght;
-
-    //Дубляж кода - исправить
-    //++++++++++++++++++++++++++++++++
-    const canvas = document.getElementById("treeJSScene");
-    var render = new THREE.WebGLRenderer({ canvas: canvas });
-    render.setClearColor(0x000000);
+    const widthScrenn = window.innerWidth;
+    const heightScrenn = window.innerHeight;
+    const canvasScrenn = document.getElementById('treeJSScene');
+   
+    var render = new THREE.WebGLRenderer({ canvas: canvasScrenn });
+    render.setSize(widthScrenn, heightScrenn);
+    render.setClearColor(0x808080);
 
     var scene = new THREE.Scene();
 
-    var light = new THREE.AmbientLight(0xffffff);
+    var camera = new THREE.PerspectiveCamera(60, widthScrenn / heightScrenn, 0.1, 15000);
+    camera.position.set(500, 50, 2500);
+
+    var light = new THREE.AmbientLight(0x000000);
     scene.add(light);
 
-    const geometry = new THREE.BoxGeometry(1, heightFooter, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x344500 });
-    //++++++++++++++++++++++++++++++++
-
-    let stepLenghtFooter = stepLenght;
-    let stepWidthFooter = stepWidth;
-
-    let countWidth = widthFooter / stepWidthFooter;
-    let countLenght = lenghtFooter / stepLenghtFooter;
+    const geometry = new THREE.BoxGeometry(100, 300, 100);
+    const material = new THREE.MeshBasicMaterial({ color: 0xaa0000});
 
 
-    for (let lenghtIndex = 0; lenghtIndex < Math.floor(countLenght);lenghtIndex++) {
-        for (let widthIndex = 0; widthIndex < Math.floor(countWidth); widthIndex++) {
-            
+    let arrayCube = [];
+    for (let i = 0; i < 15; i++) {
+        for (let j = 0; j < 8; j++) {
             var cube = new THREE.Mesh(geometry, material);
-            cube.position = new Vector3(lenghtIndex * stepLenghtFooter, 0, widthIndex * stepWidthFooter);
+            cube.position.set(i*600,  
+                10,
+                j*1200);
+            arrayCube.push(cube);
             scene.add(cube);
         }
     }
+
+    function renderAnimation2() {
+        requestAnimationFrame(renderAnimation2);
+        for (let index = 0; index < arrayCube.length; index++) {
+            arrayCube[index].rotation.x += 0.01;
+            arrayCube[index].rotation.y += 0.01;
+        }
+        render.render(scene, camera);
+    }
+
+    
+    let ix = 0;
+    let iz = 0;
+    let backX = false;
+    let backZ = false;
+    let speed = 10;
+    function animationCamera() {
+        
+        if (ix <= 3500 && !backX) {
+            ix += speed*10;
+        } else {
+            backX = true;
+            ix -= speed*10;
+            if (ix <= 0)
+                backX = false;
+        }
+        if (iz <= 3500 && !backZ) {
+            iz += speed ;
+        } else {
+            backZ = true;
+            iz -= speed;
+            if (iz <= 0)
+                backZ = false;
+            camera.position.set(ix, 50, iz);
+            render.render(scene, camera);
+        }
+        requestAnimationFrame(animationCamera);
+
+    }
+
+    
+    animationCamera();
+    renderAnimation2();
 }
 
 
